2e2337c46e511a11050ef0a58af8a0d40ee60d40
recolecciontf <recolecciontf@gmail.com>
2026-08-07 12:05:44 +0200

    Actualiza la PWA y fuerza la nueva versión

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "compras-de-campo-v7";
+const CACHE_NAME = "compras-de-campo-v8";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
