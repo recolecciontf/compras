1f33052d7cfd327c15dfba3bdf11ba317fb2c113
recolecciontf <recolecciontf@gmail.com>
2026-08-10 07:09:50 +0200

    Actualiza gestión segura de expedientes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "compras-de-campo-v13";
+const CACHE_NAME = "compras-de-campo-v14";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
